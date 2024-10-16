f4495cf6e51ecdc2a1cc479821e35120891d12eb
Luiz Fernando <113379005+luuizfernando@users.noreply.github.com>
2024-10-16 14:31:58 -0300

    otimizacao

--- src/app.js
+++ src/app.js
@@ -26,10 +26,10 @@ const formatado = agora.format('HH:mm:ss');
 app.post("/participants", async (req, res) => {
     const { name } = req.body;
 
-    const participantsSchema = joi.object({
+    const participantSchema = joi.object({
         name: joi.string().required()
     });
-    const validation = participantsSchema.validate(req.body, { abortEarly: false });
+    const validation = participantSchema.validate(req.body, { abortEarly: false });
     if (validation.error) {
         const errors = validation.error.details.map(detail => detail.message);
         return res.status(422).send(errors);
@@ -39,23 +39,22 @@ app.post("/participants", async (req, res) => {
     const newMessage = { from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: `${formatado}` };
 
     try {
-        const usuario = await db.collection("participants").findOne(newUser);
-        if (usuario) return res.status(409).send("Esse usuário já existe.");
+        const participant = await db.collection("participants").findOne({ name });
+        if (participant) return res.status(409).send("Esse usuário já existe.");
 
         await db.collection("participants").insertOne(newUser);
-        res.sendStatus(201);
 
         await db.collection("messages").insertOne(newMessage);
         res.sendStatus(201);
     } catch (err) {
-        console.log(err.message);
+        res.status(500).send(err.message);
     }
 });
 
 app.get("/participants", async (req, res) => {
     try {
-        const participantes = await db.collection("participants").find().toArray();
-        res.send(participantes);
+        const participants = await db.collection("participants").find().toArray();
+        res.send(participants);
     } catch (err) {
         res.status(500).send(err.message);
     }
@@ -91,12 +90,27 @@ app.post("/messages", async (req, res) => {
     }
 });
 
-app.get("/messages", (req, res) => {
+app.get("/messages", async (req, res) => {
 
 });
 
-app.post("/status", (req, res) => {
+app.post("/status", async (req, res) => {
+    const { User } = req.headers;
 
+    const headerSchema = joi.object({
+        User: joi.string().required()
+    });
+    const validation = headerSchema.validate(User, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map(detail => detail.message);
+        return res.status(404).send(errors);
+    }
+
+    try {
+
+    } catch (err) {
+
+    }
 });
 
 app.listen(process.env.PORT, () => { console.log(`Servidor Rodando na porta ${process.env.PORT}.`) });
